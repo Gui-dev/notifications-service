a6bec800386ad28e78708f0578b848b849e725ae
Gui-dev <adrianon10@gmail.com>
2022-12-22 03:14:19 -0300

    feat: add controller Notification

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
 
+import { NotificationController } from './notifications/notification.controller'
 import { PrismaService } from './prisma/prisma.service'
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [NotificationController],
   providers: [PrismaService],
 })
 export class AppModule { }
