fc6de691163fa62167e6c777654b239f7276757f
Gui-dev <adrianon10@gmail.com>
2022-12-22 04:27:47 -0300

    test: it should not be able to create a notification content with less than 5 characters

--- src/app/entities/notification/content.spec.ts
+++ src/app/entities/notification/content.spec.ts
@@ -1,6 +1,10 @@
 import { Content } from './content'
 
 describe('Content class', () => {
+  it('should not be able to create a notification content with less than 5 characters', () => {
+    expect(() => new Content('AAA')).toThrow()
+  })
+
   it('should be able to create a notification content', () => {
     const content = new Content('Você recebeu uma nova notificação')
     expect(content).toBeTruthy()
