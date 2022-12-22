79fcc0d0a61bf4638013aad38e6f684e3ffeaceb
Gui-dev <adrianon10@gmail.com>
2022-12-22 04:28:26 -0300

    test: it should not be able to create a notification content with more than 240 characters

--- src/app/entities/notification/content.spec.ts
+++ src/app/entities/notification/content.spec.ts
@@ -5,6 +5,10 @@ describe('Content class', () => {
     expect(() => new Content('AAA')).toThrow()
   })
 
+  it('should not be able to create a notification content with more than 240 characters', () => {
+    expect(() => new Content('A'.repeat(241))).toThrow()
+  })
+
   it('should be able to create a notification content', () => {
     const content = new Content('Você recebeu uma nova notificação')
     expect(content).toBeTruthy()
