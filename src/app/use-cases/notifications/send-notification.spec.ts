9295e842696b8e8ad644fde6fc9ba7246ce99669
Gui-dev <adrianon10@gmail.com>
2022-12-23 04:33:14 -0300

    test: it should be able to create a notification

---
+++ src/app/use-cases/notifications/send-notification.spec.ts
@@ -0,0 +1,17 @@
+import { InMemoryNotificationsRepository } from './../../../../test/repositories/notification/in-memory-notifications-repository'
+import { SendNotification } from './send-notification'
+
+
+describe('Send Notification', () => {
+  it('should be able to create a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository()
+    const sendNotification = new SendNotification(notificationsRepository)
+    const { notification } = await sendNotification.execute({
+      recipientId: 'example-recipient-id',
+      category: 'social',
+      content: 'Nova solicitação de amizade'
+    })
+    expect(notificationsRepository.notifications).toHaveLength(1)
+    expect(notificationsRepository.notifications[0]).toEqual(notification)
+  })
+})
