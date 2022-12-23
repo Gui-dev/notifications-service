ff788ede2332714b752b53b78048067c0742f6dd
Gui-dev <adrianon10@gmail.com>
2022-12-23 03:25:29 -0300

    feat: add helper Replace

--- src/app/entities/notification/notification.ts
+++ src/app/entities/notification/notification.ts
@@ -1,3 +1,4 @@
+import { Replace } from 'src/helpers/Replace'
 import { Content } from './content'
 
 export interface INotification {
@@ -11,8 +12,11 @@ export interface INotification {
 export class Notification {
   private props: INotification
 
-  constructor(props: INotification) {
-    this.props = props
+  constructor(props: Replace<INotification, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date()
+    }
   }
 
   public set recipientId (recipientId: string) {
