615f29d14dd3efe133425e1209dfac7bf9f96a31
Gui-dev <adrianon10@gmail.com>
2022-12-24 04:11:22 -0300

    feat: add controler notification

---
+++ src/infra/http/controllers/notifications/notification.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post } from '@nestjs/common'
+
+import { CreateNotificationBody } from '../../dtos/create-notification-body'
+import { SendNotification } from './../../../../app/use-cases/notifications/send-notification'
+
+@Controller('notifications')
+export class NotificationController {
+  constructor(private readonly sendNotification: SendNotification) { }
+
+  @Post()
+  public async create (@Body() body: CreateNotificationBody) {
+    const { recipientId, category, content } = body
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      category,
+      content
+    })
+
+    return notification
+  }
+
+}
