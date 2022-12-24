41a81b356ebabcb7f0409e8b7448dbd887d551be
Gui-dev <adrianon10@gmail.com>
2022-12-24 04:13:56 -0300

    feat: add repository notification

---
+++ src/infra/database/prisma/repositories/notifications/prisma-notifications-repository.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common'
+
+import { PrismaService } from '../../prisma.service'
+import { Notification } from './../../../../../app/entities/notification/notification'
+import { NotificationsRepository } from './../../../../../app/repositories/notifications/notifications-repository'
+
+@Injectable()
+export class PrismaNotificationsRepository implements NotificationsRepository {
+  constructor(
+    private readonly prisma: PrismaService
+  ) { }
+
+  public async create (notification: Notification): Promise<void> {
+    await this.prisma.notification.create({
+      data: {
+        id: notification.id,
+        recipientId: notification.recipientId,
+        category: notification.category,
+        content: notification.content.value,
+        readAt: notification.readAt,
+        createdAt: notification.createdAt
+      }
+    })
+  }
+
+}
