329aa3ff9470d31bde5d9766e0ba56a906ace5cf
Gui-dev <adrianon10@gmail.com>
2022-12-27 03:58:53 -0300

    test: it should be able to cancel a notification

---
+++ src/app/use-cases/notifications/cancel-notification.spec.ts
@@ -0,0 +1,25 @@
+import { Content } from '@app/entities/notification/content'
+import { Notification } from '@app/entities/notification/notification'
+import { InMemoryNotificationsRepository } from '@test/repositories/notification/in-memory-notifications-repository'
+import { CancelNotification } from './cancel-notification'
+import { NotificationNotFound } from './errors/notification-not-found'
+
+
+describe('Cancel Notification', () => {
+  it('should be able to cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository()
+    const cancelNotification = new CancelNotification(notificationsRepository)
+    const notification = new Notification({
+      recipientId: 'example-recipient-id',
+      category: 'social',
+      content: new Content('Nova solicitação de amizade')
+    })
+
+    await notificationsRepository.create(notification)
+
+    await cancelNotification.execute({
+      notificationId: notification.id
+    })
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(expect.any(Date))
+  })
+})
