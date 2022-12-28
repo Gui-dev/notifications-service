4776d55421b2eca40b7981de69bffc6c7a1a7371
Gui-dev <adrianon10@gmail.com>
2022-12-28 04:12:56 -0300

    test: it should not be able to read a non existing notification

--- src/app/use-cases/notifications/read-notification.spec.ts
+++ src/app/use-cases/notifications/read-notification.spec.ts
@@ -1,6 +1,7 @@
 import { makeNotification } from '@test/factories/notification-factory'
 import { InMemoryNotificationsRepository } from '@test/repositories/notification/in-memory-notifications-repository'
 import { ReadNotification } from './read-notification'
+import { NotificationNotFound } from './errors/notification-not-found'
 
 describe('Read Notification', () => {
   it('should be able to read a notification', async () => {
@@ -14,5 +15,16 @@ describe('Read Notification', () => {
       notificationId: notification.id
     })
     expect(notificationsRepository.notifications[0].readAt).toEqual(expect.any(Date))
+  })
+
+  it('should not be able to read a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository()
+    const readNotification = new ReadNotification(notificationsRepository)
+
+    expect(async () => {
+      return await readNotification.execute({
+        notificationId: 'fake-notification-id'
+      })
+    }).rejects.toThrow(NotificationNotFound)
   })
 })
