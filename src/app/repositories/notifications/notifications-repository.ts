957b2eb980d0ffd2ee458e75c8cde23645b91b49
Gui-dev <adrianon10@gmail.com>
2022-12-30 03:36:17 -0300

    feat: add new abstract methods

--- src/app/repositories/notifications/notifications-repository.ts
+++ src/app/repositories/notifications/notifications-repository.ts
@@ -1,6 +1,9 @@
 import { Notification } from '@app/entities/notification/notification'
 
-
 export abstract class NotificationsRepository {
   abstract create (notification: Notification): Promise<void>
+  abstract findById (notificationId: Notification['id']): Promise<Notification | null>
+  abstract save (notification: Notification): Promise<void>
+  abstract countManyByRecipientId (recipientId: Notification['recipientId']): Promise<number>
+  abstract findManyByRecipientId (recipientId: Notification['recipientId']): Promise<Notification[]>
 }
