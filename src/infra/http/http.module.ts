85e370a2fc4f3a576f1893fe06cd803dca4d877c
Gui-dev <adrianon10@gmail.com>
2022-12-30 04:22:38 -0300

    feat: add new providers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,10 +3,22 @@ import { DatabaseModule } from '../database/database.module'
 import { SendNotification } from '@app/use-cases/notifications/send-notification'
 
 import { NotificationController } from './controllers/notifications/notification.controller'
+import { CancelNotification } from '@app/use-cases/notifications/cancel-notification'
+import { ReadNotification } from '@app/use-cases/notifications/read-notification'
+import { UnreadNotification } from '@app/use-cases/notifications/unread-notification'
+import { CountRecipientNotifications } from '@app/use-cases/notifications/count-recipient-notifications'
+import { GetRecipientNotifications } from '@app/use-cases/notifications/get-recipient-notifications'
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationController],
-  providers: [SendNotification]
+  providers: [
+    SendNotification,
+    CancelNotification,
+    ReadNotification,
+    UnreadNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications
+  ]
 })
 export class HttpModule { }
