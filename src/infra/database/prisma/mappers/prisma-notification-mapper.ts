f2523c9d3cd5c4a9b4fe9a5e9d5b793e69d21abb
Gui-dev <adrianon10@gmail.com>
2022-12-30 03:38:21 -0300

    feat: add toDomain static method

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotification } from '@prisma/client'
 import { Notification } from '@app/entities/notification/notification'
+import { Content } from '@app/entities/notification/content'
 
 export class PrismaNotificationMapper {
   static toPrisma (notification: Notification) {
@@ -10,5 +12,16 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt
     }
+  }
+
+  static toDomain (raw: RawNotification): Notification {
+    return new Notification({
+      recipientId: raw.recipientId,
+      category: raw.category,
+      content: new Content(raw.content),
+      readAt: raw.readAt,
+      canceledAt: raw.canceledAt,
+      createdAt: raw.createdAt
+    }, raw.id)
   }
 }
